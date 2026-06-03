0ce3179cbf47c51191088750c4ae7dace1c378f4
damian2340 <damian2340@hotmail.com>
2026-06-03 19:50:41 -0300

    fuentes a la pantalla

--- firmware/Controlador-de-temperatura/Core/Src/tasks/task_ui.c
+++ firmware/Controlador-de-temperatura/Core/Src/tasks/task_ui.c
@@ -10,7 +10,10 @@ void task_ui(void)
     update_counter++;
     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
     if (update_counter >= 50) {
-        oled_hw_test_pattern();
+        float temp = adc_hw_read_temperature();
+        oled_hw_clear();
+        oled_hw_print_float(temp, 2);
+        oled_hw_update();
         update_counter = 0;
     }
 }
