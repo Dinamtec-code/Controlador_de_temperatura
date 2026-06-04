a9e0c81205dcebcefa973f00348d0870a6fc0e33
Dinamtec-code <dinamtec-code@users.noreply.github.com>
2026-06-03 21:37:50 -0300

    correccion del antiwindup

--- firmware/Controlador-de-temperatura/Core/Inc/control/pid_controller.h
+++ firmware/Controlador-de-temperatura/Core/Inc/control/pid_controller.h
@@ -1,27 +1,24 @@
 #ifndef PID_CONTROLLER_H
 #define PID_CONTROLLER_H
 
-#include <stdint.h>
+#define ANTI_WINDUP 1
 
-typedef struct {
-    float kp;
-    float ki;
-    float kd;
-    float setpoint;
-    float integral;
-    float previous_error;
+#include "arm_math_types.h"
+#include "control/controller_functions.h"
+
+typedef struct
+{
+    arm_pid_instance_f32 cmsis_pid;
     float output_limit_min;
     float output_limit_max;
-    float integral_limit_min;
-    float integral_limit_max;
     uint8_t initialized;
+    float prev_output;
 } pid_controller_t;
 
 void pid_init(pid_controller_t *pid, float kp, float ki, float kd);
 void pid_set_parameters(pid_controller_t *pid, float kp, float ki, float kd);
-void pid_set_limits(pid_controller_t *pid, float out_min, float out_max, float int_min, float int_max);
-void pid_set_setpoint(pid_controller_t *pid, float setpoint);
-float pid_compute(pid_controller_t *pid, float input);
+void pid_set_limits(pid_controller_t *pid, float out_min, float out_max);
+float pid_compute(pid_controller_t *pid, float setpoint, float input);
 void pid_reset(pid_controller_t *pid);
 
 #endif
