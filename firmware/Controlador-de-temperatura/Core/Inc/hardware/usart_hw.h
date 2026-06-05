291ff3773b56b1bfa63d5076666526cbb49786bc
Dinamtec-Code <damian2340spam01@gmail.com>
2026-06-05 14:21:10 -0300

    refactorizacion completa

--- firmware/Controlador-de-temperatura/Core/Inc/hardware/usart_hw.h
+++ firmware/Controlador-de-temperatura/Core/Inc/hardware/usart_hw.h
@@ -7,14 +7,20 @@
 #include "hardware/circular_buffer.h"
 
 #define USART_HW_RX_BUFFER_SIZE 128
+#define USART_HW_TX_BUFFER_SIZE 64
 
+/* USART handle externo */
 extern UART_HandleTypeDef huart2;
+
+/* Buffer de recepción externo para task_comm */
 extern circular_buffer_t rx_circular_buffer;
 
+/* API pública */
 void usart_hw_init(void);
 void usart_hw_start_rx(void);
 void usart_hw_idle_handler(void);
 void usart_hw_send_str(const char *str);
 void usart_hw_send_buf(uint8_t *data, size_t len);
+bool usart_hw_is_tx_ready(void);
 
 #endif
--- firmware/Controlador-de-temperatura/Core/Inc/usart.h
+++ firmware/Controlador-de-temperatura/Core/Inc/usart.h
@@ -1,21 +1,21 @@
 /**
- ******************************************************************************
- * @file    usart.h
- * @brief   This file contains all the function prototypes for
- *          the usart.c file
- ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; Copyright (c) 2026 STMicroelectronics.
- * All rights reserved.</center></h2>
- *
- * This software component is licensed by ST under Ultimate Liberty license
- * SLA0044, the "License"; You may not use this file except in compliance with
- * the License. You may obtain a copy of the License at:
- *                             www.st.com/SLA0044
- *
- ******************************************************************************
- */
+  ******************************************************************************
+  * @file    usart.h
+  * @brief   This file contains all the function prototypes for
+  *          the usart.c file
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __USART_H__
 #define __USART_H__
@@ -35,66 +35,23 @@ extern "C"
   extern UART_HandleTypeDef huart2;
   extern DMA_HandleTypeDef hdma_usart2_rx;
   extern DMA_HandleTypeDef hdma_usart2_tx;
-  extern uint8_t dma_uart_rx_buffer[];
 
 /* USER CODE BEGIN Private defines */
-#define DMA_TX_BUFFER_SIZE 64
+
 #define DMA_RX_BUFFER_SIZE 64
-#define SDIN_BUFFER_SIZE 64
 
-  typedef enum State
-  {
-    BUFFER_idle = 0,
-    BUFFER_busy
-  } BufferStateTypeDef;
-
-  typedef struct C_BUFFER_HANDLER
-  {
-    uint8_t *BufferStart_p;
-    size_t BufferLength;
-    size_t BufferStartOffset; /* El encargado de cambiar el offset es la interrupci�n sobre el final de la transmisi�n*/
-    size_t BufferEndOffset;   /* Es la posici�n donde se va a escribir el pr�ximo dato */
-    size_t ToSend;
-    size_t BufferNextStartOffset;
-    size_t BufferDataLength; /* Usado para indicarle al modulo DMA cuantos datos se van a enviar */
-    size_t DataSize;
-    BufferStateTypeDef BufferState;
-  } cBufferHandler_t;
-
-  extern cBufferHandler_t TxbufferHandler;
-
-  typedef enum BUFFER_TYPE
-  {
-    dataBufferType,
-    sdoutBufferType,
-    errorBufferType,
-    sdinBufferType
-  } bufferType_t;
-
-  /* USER CODE END Private defines */
+/* USER CODE END Private defines */
 
   void MX_USART2_UART_Init(void);
 
-  /* USER CODE BEGIN Prototypes */
-  void TransmitionStart(cBufferHandler_t *);
-  HAL_StatusTypeDef sendChar(char ch);
-
-  void comm_task(void);
-  void receptionStart();
-  void uartSetReady();
-  void uartOnReceive();
-  void uartConsumeData();
-  HAL_UART_StateTypeDef uartReceive();
-  uint8_t *uartGetData();
-  uint8_t uartIsReady();
-  BufferStateTypeDef sdinIsBusy();
-
-  /* USER CODE END Prototypes */
+/* USER CODE BEGIN Prototypes */
+/* Functions for hardware abstraction layer */
+void receptionStart(void);
+extern uint8_t dma_uart_rx_buffer[];
+/* USER CODE END Prototypes */
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* __USART_H__ */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
